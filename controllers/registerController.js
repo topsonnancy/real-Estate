169c54fb32e56e2817e4a748908cd4e12ff06a3a
temitope fadipe <temitopefadipe@yahoo.com>
2024-03-26 11:15:16 +0100

    Mail functionality in register controller

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,25 +1,94 @@
 const User = require("../models/User");
-const CryptoJs = require("crypto-js")
+const CryptoJs = require("crypto-js");
+const nodemailer = require("nodemailer");
 
 const handleRegister = async (req, res) => {
-    const {user, contact, email, image, pwd} = req.body
+  const { user, contact, email, image, pwd } = req.body;
 
-    if (!user ||!contact || !email || !pwd) return res.status(401).json("Field cannot be empty")
-    try {
-        encryptPwd = CryptoJs.AES.encrypt(pwd, process.env.HASHEDPWD)
-        const newUser = await User.create({
-            username: user,
-            contact: contact,
-            email: email,
-            image: image,
-            password: encryptPwd   
-        }) 
-             
+  if (!user || !contact || !email || !pwd)
+    return res.status(401).json("Field cannot be empty");
+
+  try {
+    // Encrypt password
+    const encryptPwd = CryptoJs.AES.encrypt(pwd, process.env.HASHEDPWD);
+
+    // Create new user
+    const newUser = await User.create({
+      username: user,
+      contact: contact,
+      email: email,
+      image: image,
+      password: encryptPwd,
+    });
+
+    // Create Nodemailer transporter
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.USER,
+        pass: process.env.APP_PASSWORD,
+      },
+    });
+
+    // Define email options
+    const mailOptions = {
+      from: {
+        name: "Tem Homes",
+        address: process.env.USER,
+      },
+      to: email,
+      subject: "Welcome to Tem Homes",
+      text: "Welcome to the Team",
+      html: "<b>Welcome on board!</b>\n\n <p> We are glad to have you here</p>",
+    };
+
+    // Send email
+    await transporter.sendMail(mailOptions);
+
     res.status(200).json(newUser);
 
-    } catch (error) {
-     res.status(500).json(`Error: ${error.message}`);
-    }
-}
+  } catch (error) {
+    res.status(500).json(`Error: ${error.message}`);
+  }
+};
 
-module.exports = {handleRegister}
+module.exports = {handleRegister};
+
+
+
+
+
+
+
+
+
+
+
+
+// const User = require("../models/User");
+// const CryptoJs = require("crypto-js")
+
+
+
+// const handleRegister = async (req, res) => {
+//     const {user, contact, email, image, pwd} = req.body
+
+//     if (!user ||!contact || !email || !pwd) return res.status(401).json("Field cannot be empty")
+//     try {
+//         encryptPwd = CryptoJs.AES.encrypt(pwd, process.env.HASHEDPWD)
+//         const newUser = await User.create({
+//             username: user,
+//             contact: contact,
+//             email: email,
+//             image: image,
+//             password: encryptPwd   
+//         }) 
+             
+//     res.status(200).json(newUser);
+
+//     } catch (error) {
+//      res.status(500).json(`Error: ${error.message}`);
+//     }
+// }
+
+// module.exports = {handleRegister}
--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require("dotenv").config()
 const express = require("express");
 const mongoose = require("mongoose");
 const connectDB = require("./config/dbConfig");
-const nodemailer = require('nodemailer');
 
 // initialize express
 const app = express();
